d1b82f3368654e88632e60db735c9188e13517f5
Ali <ali.haydaroglu@mail.utoronto.ca>
2021-12-07 21:35:31 +0000

    added angles and stuff

--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,5 @@
 import numpy as n
+from matplotlib import pyplot as plt
 
 
 def dot(a, b):
@@ -18,3 +19,17 @@ def get_angle(a, b):
     # invert the angles for counter-clockwise rotations
     angle[is_cc] = 2*n.pi - angle[is_cc]
     return 360 - n.rad2deg(angle)
+
+def compare_angles(ang1, ang2, reduce=False):
+    if reduce: diff = n.min([(ang1-ang2)%360, 360-((ang1-ang2)%360)], axis=0)
+    else: diff = (ang1 - ang2) % 360
+    return diff
+
+def plot_rad_hist(diff,n_bins=50, bin_width=0.1, figsize=(8,8), title=None):
+    f, ax = plt.subplots(figsize=figsize, subplot_kw = {'projection' : 'polar'})
+    hist, bins = n.histogram(diff, bins=n_bins, range=(0,360))
+    bins = [(bins[i]+bins[i+1])/2 for i in range(len(bins)-1)] 
+    ax.set_rgrids([])
+    bars = ax.bar(n.deg2rad(bins), hist, width=bin_width)
+    ax.set_title(title)
+    return f, bins, hist
